043adf2b349e230ee9f3d6ebe31888ff68a97e2c
MIsrarAhmad <m.israrahmad2021@gmail.com>
2024-07-15 05:44:43 -0700

    finally done my remain asignment

---
+++ script.js
@@ -0,0 +1,60 @@
+// task 1
+// let string = "Hello World";
+// extra= string.trim();
+// console.log(extra);
+
+// // task 2
+// let string = "Hello World";
+// substring = string.substring(5,10);
+// console.log(substring);
+
+// // task 3
+// let string = "Hello World";
+// str = string.substring(0, 5);    //str = string.substr(0, 5);
+// console.log(str);
+
+// // task 4
+// let string = "Hello-World";
+// let splitstr = string.split("-");
+// console.log(splitstr);
+
+// // task 5
+// let array = ["hello","word"];
+// let joinstr = array.join(" ");
+// console.log(joinstr);
+
+// task 6
+// let string = "hello World"
+// let str = string.indexOf("o");
+// console.log(str);
+
+// task 7
+// let string = "hello World World";
+// let str = string.lastIndexOf('World');
+// console.log(str);
+
+
+// // task 8
+// let string = "World";
+// let str = string.startsWith('Wor');
+// console.log(str);
+
+// task 9
+// let string = "Hello World";
+// let str = string.endsWith('World');
+// console.log(str);
+
+// // Task 10
+// let string = "Hello";
+// let str = string.includes('o');
+// console.log(str);
+
+
+
+
+
+
+
+
+
+
